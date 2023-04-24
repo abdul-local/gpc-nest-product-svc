248a59ac83d9ba8d1216cd3beab2922ce65fcb98
Abdullah Hamzan <abdullah.hamzan@gmail.com>
2023-04-24 14:41:22 +0800

    add orm prisma

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,21 +1,20 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProductRequestDto } from './product.dto';
-import { Product, dataDummyProduct } from './utils';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
+  constructor(private readonly prismaService: PrismaService) {}
   public async createProduct(payload: CreateProductRequestDto): Promise<any> {
-    const product: any = new Product();
-
-    product.name = payload.name;
-    product.sku = payload.sku;
-    product.stock = payload.stock;
-    product.price = payload.price;
+    const product: any = await this.prismaService.products.create({
+      data: payload,
+    });
 
     return { id: product.id, error: null, status: HttpStatus.OK };
   }
 
   public async getAllProduct(): Promise<any> {
-    return { statusCode: HttpStatus.OK, data: dataDummyProduct };
+    const allProduct: any = await this.prismaService.products.findMany();
+    return { statusCode: HttpStatus.OK, data: allProduct };
   }
 }
